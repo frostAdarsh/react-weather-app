71edb6fb0e2f4efddbdcbc6f891b0a6a283b3d7c
frostAdarsh <adarshmani017@gmail.com>
2024-06-09 22:23:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Weather from './Components/Weather'
+
+function App() {
+  
+
+  return (
+    <>
+     <div className="app">
+      <Weather/>
+     </div>
+    </>
+  )
+}
+
+export default App
